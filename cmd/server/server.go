7012ec54c971110ce8771887272c9b59bfb1fbd5
Michael Holtzscher <michael@holtzscher.com>
2024-06-26 21:52:18 -0500

    remove old test code from server cmd

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,10 +3,8 @@ package main
 import (
 	"context"
 	"log"
-	"reflect"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/mholtzscher/formula-data/internal/dal"
 )
 
@@ -20,32 +18,8 @@ func run() error {
 	defer conn.Close(ctx)
 
 	queries := dal.New(conn)
+	_ = queries
 
-	// list all authors
-	authors, err := queries.ListAuthors(ctx)
-	if err != nil {
-		return err
-	}
-	log.Println(authors)
-
-	// create an author
-	insertedAuthor, err := queries.CreateAuthor(ctx, dal.CreateAuthorParams{
-		Name: "Brian Kernighan",
-		Bio:  pgtype.Text{String: "Co-author of The C Programming Language and The Go Programming Language", Valid: true},
-	})
-	if err != nil {
-		return err
-	}
-	log.Println(insertedAuthor)
-
-	// get the author we just inserted
-	fetchedAuthor, err := queries.GetAuthor(ctx, insertedAuthor.ID)
-	if err != nil {
-		return err
-	}
-
-	// prints true
-	log.Println(reflect.DeepEqual(insertedAuthor, fetchedAuthor))
 	return nil
 }
 
