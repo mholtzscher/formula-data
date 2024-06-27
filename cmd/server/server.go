535c608ad1f994dc730e711ca06b25d82a7ce2f9
Michael Holtzscher <michael@holtzscher.com>
2024-06-26 23:02:10 -0500

    add zerolog

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"context"
-	"log"
+	"flag"
 	"net/http"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/mholtzscher/formula-data/gen/api/v1/apiv1connect"
 	"github.com/mholtzscher/formula-data/internal/dal"
 	srvV1 "github.com/mholtzscher/formula-data/internal/service/v1"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
+var debug = flag.Bool("debug", false, "sets log level to debug")
+
 func main() {
+	flag.Parse()
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	log.Info().Msg("Starting server...")
+
 	ctx := context.Background()
 
 	conn, err := pgx.Connect(ctx, "host=localhost user=postgres password=postgres dbname=formula-data")
 	if err != nil {
-		log.Fatal("could not connect to database: ", err)
+		log.Fatal().Err(err).Msg("could not connect to database")
 	}
 	defer conn.Close(ctx)
 
--- go.mod
+++ go.mod
@@ -12,6 +12,10 @@ require (
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 )
