c2fcbbba1c235908d6aae7d077c338b0a5cfdf1f
Michael Holtzscher <michael@holtzscher.com>
2024-07-05 00:06:40 -0500

    upgrade postgres version

--- internal/testing/integration/container.go
+++ internal/testing/integration/container.go
@@ -86,7 +86,7 @@ func runMigrations(db *sql.DB) {
 func createPostgresContainer(t *testing.T, ctx context.Context) (*postgres.PostgresContainer, string) {
 	t.Helper()
 	pgContainer, err := postgres.RunContainer(ctx,
-		testcontainers.WithImage("postgres:15.3-alpine"),
+		testcontainers.WithImage("postgres:16-alpine"),
 		postgres.WithDatabase("test-db"),
 		postgres.WithUsername("postgres"),
 		postgres.WithPassword("postgres"),
