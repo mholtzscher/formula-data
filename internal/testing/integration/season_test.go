9e5462fafdecf38b9ade05dc7bbca6383c26264b
Michael Holtzscher <michael@holtzscher.com>
2024-07-04 22:33:38 -0500

    ```
    refactor: restructure test suite and add validation checks
    
    Refactor the formula data server integration tests by extracting 
    common setup code into a new helper function, `CreateTestServerAndClient`.
    This reduces redundancy and simplifies the test structure.
    
    Add several new validation tests to enforce constraints on the 
    `CreateSeason` endpoint. These validations ensure that the `year` 
    must be between 1900 and 2100 and that the `series` name is required.
    
    Implement a utility function to map Postgres errors to appropriate 
    gRPC error codes, improving error handling and client feedback.
    ```

--- internal/testing/integration/season_test.go
+++ internal/testing/integration/season_test.go
@@ -2,54 +2,77 @@ package integration
 
 import (
 	"context"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/jackc/pgx/v5"
 	apiv1 "github.com/mholtzscher/formula-data/gen/api/v1"
-	"github.com/mholtzscher/formula-data/gen/api/v1/apiv1connect"
-	"github.com/rs/zerolog/log"
-
-	"github.com/mholtzscher/formula-data/internal/dal"
-	srvV1 "github.com/mholtzscher/formula-data/internal/service/v1"
 
 	"connectrpc.com/connect"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestFormulaDataServer_Season(t *testing.T) {
-	ctx := context.Background()
-	container := CreatePostgresContainer(ctx)
-	conn, err := pgx.Connect(ctx, container.ConnectionString)
-	if err != nil {
-		log.Fatal().Err(err).Msg("could not connect to database")
-	}
-	defer conn.Close(ctx)
+	client := CreateTestServerAndClient(t)
 
-	queries := dal.New(conn)
-	fdServer := srvV1.NewFormulaDataServer(queries)
+	t.Run("create season should return season id", func(t *testing.T) {
+		result, err := client.CreateSeason(context.Background(), connect.NewRequest(&apiv1.CreateSeasonRequest{
+			Year:   2024,
+			Series: "Formula Test",
+		}))
+		assert.Nil(t, err)
+		assert.NotNil(t, result.Msg.SeasonId)
+	})
 
-	mux := http.NewServeMux()
-	mux.Handle(apiv1connect.NewFormulaDataServiceHandler(fdServer))
-	server := httptest.NewUnstartedServer(mux)
-	server.EnableHTTP2 = true
-	server.StartTLS()
-	defer server.Close()
+	t.Run("season should require year", func(t *testing.T) {
+		result, err := client.CreateSeason(context.Background(), connect.NewRequest(&apiv1.CreateSeasonRequest{
+			Series: "Formula Test",
+		}))
+		assert.NotNil(t, err)
+		assert.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
+		assert.Nil(t, result)
+	})
 
-	connectClient := apiv1connect.NewFormulaDataServiceClient(server.Client(), server.URL)
-	grpcClient := apiv1connect.NewFormulaDataServiceClient(server.Client(), server.URL, connect.WithGRPC())
-	clients := []apiv1connect.FormulaDataServiceClient{connectClient, grpcClient}
+	t.Run("season should be after 1900", func(t *testing.T) {
+		result, err := client.CreateSeason(context.Background(), connect.NewRequest(&apiv1.CreateSeasonRequest{
+			Year:   1900,
+			Series: "Formula Test",
+		}))
+		assert.NotNil(t, err)
+		assert.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
+		assert.Nil(t, result)
+	})
 
-	t.Run("create season", func(t *testing.T) {
-		for _, client := range clients {
-			result, err := client.CreateSeason(context.Background(), connect.NewRequest(&apiv1.CreateSeasonRequest{
-				Year:   2024,
-				Series: "Formula Test",
-			}))
-			require.Nil(t, err)
-			assert.NotNil(t, result.Msg.SeasonId)
-		}
+	t.Run("season should be before 2100", func(t *testing.T) {
+		result, err := client.CreateSeason(context.Background(), connect.NewRequest(&apiv1.CreateSeasonRequest{
+			Year:   2101,
+			Series: "Formula Test",
+		}))
+		assert.NotNil(t, err)
+		assert.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
+		assert.Nil(t, result)
+	})
+
+	t.Run("season should require series name", func(t *testing.T) {
+		result, err := client.CreateSeason(context.Background(), connect.NewRequest(&apiv1.CreateSeasonRequest{
+			Year: 2024,
+		}))
+		assert.NotNil(t, err)
+		assert.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
+		assert.Nil(t, result)
+	})
+
+	t.Run("should not allow duplicate season", func(t *testing.T) {
+		request := connect.NewRequest(&apiv1.CreateSeasonRequest{
+			Series: "Formula Test 123",
+			Year:   2024,
+		})
+
+		result, err := client.CreateSeason(context.Background(), request)
+		assert.Nil(t, err)
+		assert.NotNil(t, result.Msg.SeasonId)
+
+		result, err = client.CreateSeason(context.Background(), request)
+		assert.NotNil(t, err)
+		assert.Equal(t, connect.CodeAlreadyExists, connect.CodeOf(err))
+		assert.Nil(t, result)
 	})
 }
